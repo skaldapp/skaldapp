d035b72842de1d544b78920443366c753c0fc100
Jerry Bruwes <jbruwes@icloud.com>
2026-01-12 18:00:16 +0200

    refactor: optimize i18n setup and add Russian support

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,14 +1,13 @@
 import { defineBoot } from "#q-app/wrappers";
 import { Lang } from "quasar";
+import ru from "quasar/lang/ru";
 import messages from "src/i18n";
 import { createI18n } from "vue-i18n";
 
+const legacy = false,
+  locale = Lang.getLocale() === "ru-RU" ? "ru-RU" : "en-US";
+
 export default defineBoot(({ app }) => {
-  app.use(
-    createI18n({
-      legacy: false,
-      locale: Lang.getLocale() === "ru-RU" ? "ru-RU" : "en-US",
-      messages,
-    }),
-  );
+  if (locale === "ru-RU") Lang.set(ru);
+  app.use(createI18n({ legacy, locale, messages }));
 });
