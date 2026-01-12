a86af7e944579272f29abf4f487c99dec3da137f
Jerry Bruwes <jbruwes@icloud.com>
2026-01-12 17:48:12 +0200

    refactor: restructure eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,8 @@
 import shared from "@skaldapp/configs/eslint";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig(shared, {
-  languageOptions: { parserOptions: { tsconfigRootDir: import.meta.dirname } },
-});
+const tsconfigRootDir = import.meta.dirname,
+  parserOptions = { tsconfigRootDir },
+  languageOptions = { parserOptions };
+
+export default defineConfig(shared, { languageOptions });
