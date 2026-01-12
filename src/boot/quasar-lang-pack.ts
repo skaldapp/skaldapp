d6479f16ccae701a3edf267ed67991ebd58c716d
Jerry Bruwes <jbruwes@icloud.com>
2026-01-12 18:00:41 +0200

    chore: remove quasar-lang-pack from boot

--- src/boot/quasar-lang-pack.ts
+++
@@ -1,7 +0,0 @@
-import { consola } from "consola/browser";
-import { Lang } from "quasar";
-
-(async () => {
-  if (Lang.getLocale() === "ru-RU")
-    Lang.set((await import("quasar/lang/ru")).default);
-})().catch(consola.error);
