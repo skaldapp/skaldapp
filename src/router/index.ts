c06654e08eebbd6be4404bca8997bf0f5ceef5cf
Jerry Bruwes <jbruwes@icloud.com>
2026-01-12 18:15:32 +0200

    refactor: simplify router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,9 +3,6 @@ import { createMemoryHistory, createRouter } from "vue-router";
 
 import routes from "./routes";
 
-export default defineRouter(() =>
-  createRouter({
-    history: createMemoryHistory(process.env.VUE_ROUTER_BASE),
-    routes,
-  }),
-);
+const history = createMemoryHistory(process.env.VUE_ROUTER_BASE);
+
+export default defineRouter(() => createRouter({ history, routes }));
