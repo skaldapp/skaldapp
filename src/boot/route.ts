8a207ca879fd255efca14b3c80f06d542b8d656e
Jerry Bruwes <jbruwes@icloud.com>
2026-01-14 17:54:20 +0200

    refactor: update reactivity usage in route

--- src/boot/route.ts
+++ src/boot/route.ts
@@ -1,12 +1,16 @@
 import { defineBoot } from "#q-app/wrappers";
 import { sharedStore } from "@skaldapp/shared";
+import { storeToRefs } from "pinia";
 import routes from "src/router/routes";
-import { ioStore } from "stores/io";
+import { useIoStore } from "stores/io";
 import { useMainStore } from "stores/main";
 import { toRefs } from "vue";
 
 const [route] = routes,
+  ioStore = useIoStore(),
   mainStore = useMainStore(),
+  { reset } = ioStore,
+  { selected } = storeToRefs(mainStore),
   { tree } = toRefs(sharedStore);
 
 export default defineBoot(({ router }) => {
@@ -14,8 +18,8 @@ export default defineBoot(({ router }) => {
     if (["/", "/main"].includes(path)) next();
     else next("/");
     if (path === "/" && route) {
-      ioStore.bucket = "";
-      mainStore.selected = "";
+      reset();
+      selected.value = "";
       tree.value.length = 0;
       router.clearRoutes();
       router.addRoute(route);
