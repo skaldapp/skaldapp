e790f0b1cfadd1d50e7272313b963fa5fccad2ee
Jerry Bruwes <jbruwes@icloud.com>
2026-01-14 18:10:52 +0200

    refactor: migrate io store to pinia

--- src/stores/io.ts
+++ src/stores/io.ts
@@ -1,73 +1,87 @@
 import type { StreamingBlobPayloadInputTypes } from "@smithy/types";
 
 import * as fsa from "@skaldapp/fsa";
-import * as s3 from "stores/s3";
-import { reactive, ref, watchEffect } from "vue";
+import { acceptHMRUpdate, defineStore } from "pinia";
+import { useS3Store } from "stores/s3";
+import { ref } from "vue";
 
-let fileSystemDirectoryHandle: FileSystemDirectoryHandle | undefined,
-  remote = false;
-
-const bucket = ref(""),
-  io: () => typeof s3 | Window = () => (remote ? s3 : window);
-
-watchEffect(() => {
-  if (!bucket.value) {
-    s3.setS3Client();
+export const useIoStore = defineStore("io", () => {
+  let fileSystemDirectoryHandle: FileSystemDirectoryHandle | undefined,
     remote = false;
-    fileSystemDirectoryHandle = undefined;
-  }
+
+  const s3 = useS3Store();
+  const io = () => (remote ? s3 : window);
+  const bucket = ref(""),
+    deleteObject = async (Key: string) => {
+      if (bucket.value)
+        if (fileSystemDirectoryHandle)
+          await fsa.deleteObject(fileSystemDirectoryHandle, Key);
+        else await io().deleteObject(bucket.value, Key);
+    },
+    getObjectBlob = async (Key: string, ResponseCacheControl?: string) => {
+      if (fileSystemDirectoryHandle)
+        return fsa.getObjectBlob(fileSystemDirectoryHandle, Key);
+      return io().getObjectBlob(bucket.value, Key, ResponseCacheControl);
+    },
+    getObjectText = async (Key: string, ResponseCacheControl?: string) => {
+      if (fileSystemDirectoryHandle)
+        return fsa.getObjectText(fileSystemDirectoryHandle, Key);
+      return io().getObjectText(bucket.value, Key, ResponseCacheControl);
+    },
+    headBucket = async (Bucket: string, pin: string | undefined) => {
+      try {
+        await s3.headBucket(Bucket, pin);
+        remote = true;
+      } catch (err) {
+        const { message } = err as Error;
+        throw new Error(message);
+      }
+    },
+    headObject = async (Key: string, ResponseCacheControl?: string) => {
+      if (fileSystemDirectoryHandle)
+        return fsa.headObject(fileSystemDirectoryHandle, Key);
+      return io().headObject(bucket.value, Key, ResponseCacheControl);
+    },
+    putObject = async (
+      Key: string,
+      body: StreamingBlobPayloadInputTypes,
+      ContentType: string,
+    ) => {
+      if (bucket.value)
+        if (fileSystemDirectoryHandle)
+          await fsa.putObject(fileSystemDirectoryHandle, Key, body);
+        else await io().putObject(bucket.value, Key, body, ContentType);
+    },
+    removeEmptyDirectories = async () => {
+      const exclude = ["node_modules", ".git"];
+      if (bucket.value)
+        if (fileSystemDirectoryHandle)
+          await fsa.removeEmptyDirectories(fileSystemDirectoryHandle, exclude);
+        else await io().removeEmptyDirectories?.(bucket.value, exclude);
+    },
+    reset = () => {
+      bucket.value = "";
+      s3.setS3Client();
+      remote = false;
+      fileSystemDirectoryHandle = undefined;
+    },
+    setFileSystemDirectoryHandle = (value: FileSystemDirectoryHandle) => {
+      fileSystemDirectoryHandle = value;
+    };
+
+  return {
+    bucket,
+    deleteObject,
+    getObjectBlob,
+    getObjectText,
+    headBucket,
+    headObject,
+    putObject,
+    removeEmptyDirectories,
+    reset,
+    setFileSystemDirectoryHandle,
+  };
 });
 
-export const ioStore = reactive({
-  bucket,
-  deleteObject: async (Key: string) => {
-    if (bucket.value)
-      if (fileSystemDirectoryHandle)
-        await fsa.deleteObject(fileSystemDirectoryHandle, Key);
-      else await io().deleteObject(bucket.value, Key);
-  },
-  getObjectBlob: async (Key: string, ResponseCacheControl?: string) => {
-    if (fileSystemDirectoryHandle)
-      return fsa.getObjectBlob(fileSystemDirectoryHandle, Key);
-    return io().getObjectBlob(bucket.value, Key, ResponseCacheControl);
-  },
-  getObjectText: async (Key: string, ResponseCacheControl?: string) => {
-    if (fileSystemDirectoryHandle)
-      return fsa.getObjectText(fileSystemDirectoryHandle, Key);
-    return io().getObjectText(bucket.value, Key, ResponseCacheControl);
-  },
-  headBucket: async (Bucket: string, pin: string | undefined) => {
-    try {
-      await s3.headBucket(Bucket, pin);
-      remote = true;
-    } catch (err) {
-      const { message } = err as Error;
-      throw new Error(message);
-    }
-  },
-  headObject: async (Key: string, ResponseCacheControl?: string) => {
-    if (fileSystemDirectoryHandle)
-      return fsa.headObject(fileSystemDirectoryHandle, Key);
-    return io().headObject(bucket.value, Key, ResponseCacheControl);
-  },
-  putObject: async (
-    Key: string,
-    body: StreamingBlobPayloadInputTypes,
-    ContentType: string,
-  ) => {
-    if (bucket.value)
-      if (fileSystemDirectoryHandle)
-        await fsa.putObject(fileSystemDirectoryHandle, Key, body);
-      else await io().putObject(bucket.value, Key, body, ContentType);
-  },
-  removeEmptyDirectories: async () => {
-    const exclude = ["node_modules", ".git"];
-    if (bucket.value)
-      if (fileSystemDirectoryHandle)
-        await fsa.removeEmptyDirectories(fileSystemDirectoryHandle, exclude);
-      else await io().removeEmptyDirectories?.(bucket.value, exclude);
-  },
-  setFileSystemDirectoryHandle: (value: FileSystemDirectoryHandle) => {
-    fileSystemDirectoryHandle = value;
-  },
-});
+if (import.meta.hot)
+  import.meta.hot.accept(acceptHMRUpdate(useIoStore, import.meta.hot));
