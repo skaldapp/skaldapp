dae4ebe9fe225a98324ff4d9625cdd4a9f299d6d
Jerry Bruwes <jbruwes@icloud.com>
2026-04-08 18:38:37 +0200

    refactor(io): update S3 client implementation and object handling

--- src/stores/io.ts
+++ src/stores/io.ts
@@ -1,68 +1,141 @@
-import type { StreamingBlobPayloadInputTypes } from "@smithy/types";
+import type { GetObjectCommandInput } from "@aws-sdk/client-s3";
+import type {
+  StreamingBlobPayloadInputTypes,
+  StreamingBlobPayloadOutputTypes,
+} from "@smithy/types";
 
+import { S3 } from "@aws-sdk/client-s3";
 import * as fsa from "@skaldapp/fsa";
-import { acceptHMRUpdate, defineStore } from "pinia";
-import { useS3Store } from "stores/s3";
+import { FetchHttpHandler } from "@smithy/fetch-http-handler";
+import { AES, Utf8 } from "crypto-es";
+import { acceptHMRUpdate, defineStore, storeToRefs } from "pinia";
+import { useMainStore } from "stores/main";
 import { ref } from "vue";
 
 export const useIoStore = defineStore("io", () => {
   let fileSystemDirectoryHandle: FileSystemDirectoryHandle | undefined,
-    remote = false;
+    s3: S3 | undefined;
 
-  const s3 = useS3Store();
-  const io = () => (remote ? s3 : window);
+  const requestHandler = new FetchHttpHandler(),
+    setS3 = (value?: S3) => {
+      s3?.destroy();
+      s3 = value;
+    },
+    { credential } = storeToRefs(useMainStore());
+
   const bucket = ref(""),
     deleteObject = async (Key: string) => {
       if (bucket.value)
         if (fileSystemDirectoryHandle)
           await fsa.deleteObject(fileSystemDirectoryHandle, Key);
-        else await io().deleteObject(bucket.value, Key);
+        else await (s3 ?? window).deleteObject({ Bucket: bucket.value, Key });
     },
-    getObjectBlob = async (Key: string, ResponseCacheControl?: string) => {
-      if (fileSystemDirectoryHandle)
-        return fsa.getObjectBlob(fileSystemDirectoryHandle, Key);
-      return io().getObjectBlob(bucket.value, Key, ResponseCacheControl);
+    getObject = async ({
+      Bucket,
+      Key,
+      ResponseCacheControl,
+    }: GetObjectCommandInput) => {
+      if (Bucket !== undefined && Key !== undefined) {
+        let Body: StreamingBlobPayloadOutputTypes | undefined,
+          ContentType: string | undefined;
+        if (fileSystemDirectoryHandle)
+          ({ Body, ContentType } = await fsa.getObject(
+            fileSystemDirectoryHandle,
+            Key,
+          ));
+        else
+          try {
+            ({ Body, ContentType } = await (s3 ?? window).getObject({
+              Bucket,
+              Key,
+              ResponseCacheControl,
+            }));
+          } catch {
+            return new Response();
+          }
+        const headers = new Headers({
+          "content-type": ContentType ?? "application/octet-stream",
+        });
+        return new Response(Body as BodyInit, { headers });
+      } else return new Response();
     },
-    getObjectText = async (Key: string, ResponseCacheControl?: string) => {
-      if (fileSystemDirectoryHandle)
-        return fsa.getObjectText(fileSystemDirectoryHandle, Key);
-      return io().getObjectText(bucket.value, Key, ResponseCacheControl);
-    },
+    getObjectBlob = async (Key: string, ResponseCacheControl?: string) =>
+      (
+        await getObject({ Bucket: bucket.value, Key, ResponseCacheControl })
+      ).blob(),
+    getObjectText = async (Key: string, ResponseCacheControl?: string) =>
+      (
+        await getObject({ Bucket: bucket.value, Key, ResponseCacheControl })
+      ).text(),
     headBucket = async (Bucket: string, pin: string | undefined) => {
+      let {
+        accessKeyId = null,
+        endpoint = null,
+        region = null,
+        secretAccessKey = null,
+      } = credential.value[Bucket] ?? {};
+      if (pin) {
+        accessKeyId = AES.decrypt(accessKeyId ?? "", pin).toString(Utf8);
+        endpoint = AES.decrypt(endpoint ?? "", pin).toString(Utf8);
+        region = AES.decrypt(region ?? "", pin).toString(Utf8);
+        secretAccessKey = AES.decrypt(secretAccessKey ?? "", pin).toString(
+          Utf8,
+        );
+      }
+      setS3(
+        accessKeyId && secretAccessKey && endpoint && region
+          ? new S3({
+              credentials: { accessKeyId, secretAccessKey },
+              endpoint,
+              region,
+              requestHandler,
+            })
+          : undefined,
+      );
       try {
-        await s3.headBucket(Bucket, pin);
-        remote = true;
+        await s3?.headBucket({ Bucket });
       } catch (err) {
-        const { message } = err as Error;
-        throw new Error(message, { cause: err });
+        setS3();
+        throw new Error(err as string, { cause: err });
       }
     },
     headObject = async (Key: string, ResponseCacheControl?: string) => {
       if (fileSystemDirectoryHandle)
         return fsa.headObject(fileSystemDirectoryHandle, Key);
-      return io().headObject(bucket.value, Key, ResponseCacheControl);
+      return (s3 ?? window).headObject({
+        Bucket: bucket.value,
+        Key,
+        ResponseCacheControl,
+      });
     },
     putObject = async (
       Key: string,
       body: StreamingBlobPayloadInputTypes,
       ContentType: string,
     ) => {
-      if (bucket.value)
-        if (fileSystemDirectoryHandle)
-          await fsa.putObject(fileSystemDirectoryHandle, Key, body);
-        else await io().putObject(bucket.value, Key, body, ContentType);
+      const Body =
+        typeof body === "string" ? new TextEncoder().encode(body) : body;
+      if (fileSystemDirectoryHandle)
+        await fsa.putObject(fileSystemDirectoryHandle, Key, Body);
+      else
+        await (s3 ?? window).putObject({
+          Body,
+          Bucket: bucket.value,
+          ContentType,
+          Key,
+        });
     },
     removeEmptyDirectories = async () => {
       const exclude = ["node_modules", ".git"];
       if (bucket.value)
         if (fileSystemDirectoryHandle)
           await fsa.removeEmptyDirectories(fileSystemDirectoryHandle, exclude);
-        else await io().removeEmptyDirectories?.(bucket.value, exclude);
+        else if (!s3)
+          await window.removeEmptyDirectories(bucket.value, exclude);
     },
     reset = () => {
       bucket.value = "";
-      s3.setS3Client();
-      remote = false;
+      setS3();
       fileSystemDirectoryHandle = undefined;
     },
     setFileSystemDirectoryHandle = (value: FileSystemDirectoryHandle) => {
