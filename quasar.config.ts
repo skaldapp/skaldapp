0d59d991339a940e456fecc1d5343c52df495219
Jerry Bruwes <jbruwes@icloud.com>
2026-04-23 20:14:39 +0200

    style: reorder imports in quasar.config.ts

--- quasar.config.ts
+++ quasar.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "#q-app/wrappers";
 import { fileURLToPath } from "url";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+
+import { defineConfig } from "#q-app/wrappers";
 
 const alias = { "node:path": "path-browserify" },
   appId = "skald",
