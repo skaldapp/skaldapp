a1715449c1bd4471e46b6a187a4ae5edd86eaa6a
Jerry Bruwes <jbruwes@icloud.com>
2026-04-23 20:16:16 +0200

    refactor(stores): simplify state initialization

--- src/stores/main.ts
+++ src/stores/main.ts
@@ -2,14 +2,13 @@ import type { TCredential } from "@skaldapp/shared";
 
 import { acceptHMRUpdate, defineStore } from "pinia";
 
+const apiKey = "",
+  credential: Record<string, TCredential> = {},
+  leftDrawer = false,
+  rightDrawer = false,
+  selected = "";
 export const useMainStore = defineStore("skald", {
-  state: () => ({
-    apiKey: "",
-    credential: {} as Record<string, TCredential>,
-    leftDrawer: false,
-    rightDrawer: false,
-    selected: "",
-  }),
+  state: () => ({ apiKey, credential, leftDrawer, rightDrawer, selected }),
 });
 
 if (import.meta.hot)
