a212e51f5ae6d2b43d2d75524ed5aa864f7be7e5
Jerry Bruwes <jbruwes@gmail.com>
2026-07-23 17:34:31 +0200

    refactor: update router import paths and env variables

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 
-import { defineRouter } from "#q-app/wrappers";
+import { defineRouter } from "#q-app";
 
 import routes from "./routes";
 
-const history = createMemoryHistory(process.env.VUE_ROUTER_BASE);
+const history = createMemoryHistory(import.meta.env.QUASAR_VUE_ROUTER_BASE);
 
 export default defineRouter(() => createRouter({ history, routes }));
