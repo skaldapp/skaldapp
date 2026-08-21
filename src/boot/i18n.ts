f65b9dd1388e1f4c30a662d7d17e1479e5ac7150
Jerry Bruwes <jbruwes@gmail.com>
2026-08-21 20:11:11 +0200

    style(i18n): add TypeScript error suppression comment

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -9,6 +9,7 @@ const legacy = false,
   locale = Lang.getLocale() === "ru-RU" ? "ru-RU" : "en-US";
 
 export default defineBoot(({ app }) => {
+  // @ts-expect-error Argument of type 'QuasarLanguage' is not assignable to parameter of type '{ isoName: string; nativeName: string; rtl?: boolean; label: { clear: string; ok: string; cancel: string; close: string; set: string; select: string; reset: string; remove: string; update: string; create: string; ... 9 more ...; collapse: (label?: string | undefined) => string; }; ... 6 more ...; tree: { ...; }; }' with 'exactOptionalPropertyTypes: true'. Consider adding 'undefined' to the types of the target's properties.
   if (locale === "ru-RU") Lang.set(ru);
   app.use(createI18n({ legacy, locale, messages }));
 });
